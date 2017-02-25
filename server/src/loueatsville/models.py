a7bac323ac188fc0d01d47c543f15c26e4a75e07
Sheldon Burks <sheldon.burks@gmail.com>
2017-02-25 17:23:19 -0500

    Fixed spelling mistake in Business

--- server/src/loueatsville/models.py
+++ server/src/loueatsville/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Buisness(models.Model):
+class Business(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=300)
@@ -10,4 +10,22 @@ class Buisness(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     phone_number = models.CharField(max_length=14)
+
+
+class Violation(models.Model):
+    business = models.ForeignKey(Business)
+    date = models.DateField()
+    description = models.TextField()
+
+
+class Inspection(models.Model):
+    TYPE_CHOICES = (
+        ('FOLLOWUP', 'Follow Up'),
+        ('REGULAR', 'Regular'),
+    )
+
+    business = models.ForeignKey(Business)
+    score = models.IntegerField()
+    date = models.DateField()
+    type = models.CharField(max_length=20)
 
