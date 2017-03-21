0b8d3e002c416c099830a16ae86a10f273a30de1
Sheldon Burks <sheldon.burks@gmail.com>
2017-03-20 20:01:30 -0400

    Made several fields nullable as these were needed for the data to fit

--- server/src/loueatsville/models.py
+++ server/src/loueatsville/models.py
@@ -9,13 +9,17 @@ class Business(models.Model):
     postal_code = models.PositiveIntegerField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField(max_length=14, null=True)
+
+    @property
+    def full_address(self):
+        return '%s %s, %s, %d' % (self.address, self.city, self.state, self.postal_code)
 
 
 class Violation(models.Model):
-    business = models.ForeignKey(Business)
+    business = models.ForeignKey(Business, related_name='violations')
     date = models.DateField()
-    description = models.TextField()
+    description = models.TextField(null=True)
 
 
 class Inspection(models.Model):
@@ -25,7 +29,7 @@ class Inspection(models.Model):
     )
 
     business = models.ForeignKey(Business)
-    score = models.IntegerField()
+    score = models.IntegerField(null=True)
     date = models.DateField()
     type = models.CharField(max_length=20)
 
