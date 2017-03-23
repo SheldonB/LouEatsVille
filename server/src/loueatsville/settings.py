73faf7eddc266274c6669bb2eb34b080921356a7
Sheldon Burks <sheldon.burks@gmail.com>
2017-03-23 13:00:42 -0400

    Added Django Rest Framework as a dependency

--- server/src/loueatsville/settings.py
+++ server/src/loueatsville/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'loueatsville',
 ]
 
@@ -64,6 +65,7 @@ DATABASES = {
         'NAME': 'loueatsville',
         'USER': 'admin',
         'PASSWORD': 'password',
+        # 'HOST': 'localhost',
         'HOST': 'db',
         'PORT': 5432
     }
