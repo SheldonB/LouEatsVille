5f964076351ae4a68d443128d57c569938a79a6b
Sheldon Burks <sheldon.burks@gmail.com>
2017-03-28 11:22:40 -0400

    Initial base implementation of menu system and navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app kinda works!';
+
+  sideBarOpen:boolean = true;
+  
+  sideBarToggle() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
