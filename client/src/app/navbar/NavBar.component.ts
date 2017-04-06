b5b6efc4694302f06a873f2ce0aa54132b614cfa
Sheldon Burks <sheldon.burks@gmail.com>
2017-04-06 11:16:22 -0400

    NavBar now lives in a higher level component

---
+++ client/src/app/navbar/NavBar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './NavBar.component.html',
+  styleUrls: ['./NavBar.component.css']
+})
+export class NavBarComponent {
+  constructor() {}
+  
+  searchButtonClicked() {
+  }
+}
