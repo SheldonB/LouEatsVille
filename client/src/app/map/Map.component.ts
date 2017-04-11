12cbc2bea511130f728c2d1b3c67cf78898df06c
Sheldon Burks <sheldon.burks@gmail.com>
2017-04-11 08:35:12 -0400

    Moved assests into a specifc folder for each file type

--- client/src/app/map/Map.component.ts
+++ client/src/app/map/Map.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Business } from '../common/Business';
 import { HealthDataService } from '../common/HealthDataService';
 import { DataPanelService } from '../data-panel/DataPanel.service';
+import { NavBarService } from '../navbar/NavBar.service';
 
 @Component({
   selector: 'map',
@@ -12,26 +13,31 @@ import { DataPanelService } from '../data-panel/DataPanel.service';
 })
 export class MapComponent implements OnInit {
   businesses: Business[];
-  
-  constructor(private healthDataService : HealthDataService, private dataPanelService : DataPanelService) {}
+
+  constructor(private healthDataService : HealthDataService,
+              private dataPanelService : DataPanelService,
+              private navBarService : NavBarService) {}
 
   ngOnInit() : void {
     this.healthDataService.getBusinesses().then(businesses => this.businesses = businesses);
+    this.navBarService.applyClicked$.subscribe(searchTerm => {
+      this.healthDataService.getBusinessByName(searchTerm).then(businesses => this.businesses = businesses);
+    });
   }
 
   getPin(business : Business) : string {
     if (business.most_recent_inspection === null) {
-      return 'assets/yellow-pin.png'
+      return 'assets/img/yellow-pin.png'
     }
 
     let score = business.most_recent_inspection.score;
-
+    
     if (score === 100) {
-      return 'assets/green-pin.png';
+      return 'assets/img/green-pin.png';
     } else if (score >= 80 && score < 100) {
-      return 'assets/yellow-pin.png'
+      return 'assets/img/yellow-pin.png'
     } else {
-      return 'assets/red-pin.png';
+      return 'assets/img/red-pin.png';
     }
   }
 
