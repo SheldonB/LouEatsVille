3f5135d2d0755545859156008c8c09ff05e9282a
Sheldon Burks <sheldon.burks@gmail.com>
2017-04-20 15:47:33 -0400

    Added Gunicorn and Whitnoise for prod hosting

--- server/src/loueatsville/settings.py
+++ server/src/loueatsville/settings.py
@@ -1,30 +1,35 @@
 import os
 import json
 
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 with open('config.json') as json_file:
     config = json.load(json_file)
 
-if config['state'] == 'dev':
-    DEBUG = True
-    ALLOWED_HOSTS = []
-else:
-    DEBUG = False
-    ALLOWED_HOSTS = ['127.0.0.1']
-
-SECRET_KEY = config['secret_key']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config['database_name'],
-        'USER': config['database_user'],
-        'PASSWORD': config['database_password'],
-        'HOST': config['database_host'],
-        'PORT': 5432
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config['database_name'],
+            'USER': config['database_user'],
+            'PASSWORD': config['database_password'],
+            'HOST': config['database_host'],
+            'PORT': 5432
+        }
     }
-}
+    if config['state'] == 'dev':
+        DEBUG = True
+        ALLOWED_HOSTS = []
+    else:
+        DEBUG = False
+        ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+        db_from_env = dj_database_url.config()
+        DATABASES['default'].update(db_from_env)
+
+    SECRET_KEY = config['secret_key']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,7 +58,9 @@ ROOT_URLCONF = 'loueatsville.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +106,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 CORS_ORIGIN_ALLOW_ALL = True
--- server/src/loueatsville/urls.py
+++ server/src/loueatsville/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.views.generic.base import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from loueatsville import views
@@ -10,4 +11,5 @@ router.register(r'inspections', views.InspectionViewSet)
 
 urlpatterns = [
         url(r'^api/', include(router.urls)),
+        url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
