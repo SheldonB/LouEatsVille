e0a1e4277da0c53ffbef0cdbcb2b1875f5c63c5e
Sheldon Burks <sheldon.burks@gmail.com>
2017-04-20 23:43:14 -0400

    Modified settings

--- server/src/loueatsville/settings.py
+++ server/src/loueatsville/settings.py
@@ -93,6 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'PAGE_SIZE': 500
 }
 
