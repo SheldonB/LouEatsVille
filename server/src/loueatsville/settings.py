58ce87e17456cf69234b116e203335fc1e65cf9c
Sheldon Burks <sheldon.burks@gmail.com>
2017-05-06 12:33:03 -0500

    Revert "Modified settings"
    This reverts commit e0a1e4277da0c53ffbef0cdbcb2b1875f5c63c5e.

--- server/src/loueatsville/settings.py
+++ server/src/loueatsville/settings.py
@@ -93,9 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
     'PAGE_SIZE': 500
 }
 
